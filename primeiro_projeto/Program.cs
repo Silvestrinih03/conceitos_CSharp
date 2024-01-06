d30dc39b09d90787ecb1a77bcf0f374707eefeaf
Nicole Silvestrini <nsgarrio@gmail.com>
2024-01-06 18:17:12 -0300

    Adicionar arquivos de projeto.

---
+++ primeiro_projeto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeiro_projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Identação automárica - ctrl K D
+// Tamanho da fonte ctrl e gira o scroll do mouse
+// ctrl F5 compila o programa e roda
+
+// Tipos de dados 
