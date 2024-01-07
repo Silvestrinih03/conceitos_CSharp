0d115b75ada6e641dc1f762f1ec56e424256ed0a
Nicole Silvestrini <nsgarrio@gmail.com>
2024-01-07 11:11:43 -0300

    Comandos de repetição, vetores e matrizes

--- primeiro_projeto/Program.cs
+++ primeiro_projeto/Program.cs
@@ -13,6 +13,12 @@ namespace primeiro_projeto
     {
         static void Main(string[] args)
         {
+            //Comando DEBUGGER
+            //F9 - Habilitar/ desabilitar breakpoint
+            //F5 - Iniciar debug
+            //F10 - Rodar um passo
+            // shift F5 - Parar debug
+
             Console.WriteLine("Hello World!");
 
             // Tipos de dados e variáveis
@@ -87,36 +93,110 @@ namespace primeiro_projeto
             //Console.WriteLine(d);
 
             // Entrada de dados
-            double salario1, salario2;
-            string nome1, nome2;
-            int idade;
-            char sexo;
+            //double salario1, salario2;
+            //string nome1, nome2;
+            //int idade;
+            //char sexo;
 
-            Console.WriteLine("Digite o nome da primeira pessoa: ");
-            nome1= Console.ReadLine();
+            //Console.WriteLine("Digite o nome da primeira pessoa: ");
+            //nome1 = Console.ReadLine();
 
-            Console.WriteLine("Digite o salário da primeira pessoa: ");
-            salario1=double.Parse(Console.ReadLine(), CI);
+            //Console.WriteLine("Digite o salário da primeira pessoa: ");
+            //salario1 = double.Parse(Console.ReadLine(), CI);
 
-            Console.WriteLine("Digite o nome da segunda pessoa: ");
-            nome2=Console.ReadLine();
+            //Console.WriteLine("Digite o nome da segunda pessoa: ");
+            //nome2 = Console.ReadLine();
 
-            Console.WriteLine("Digite o salário da segunda pessoa: ");
-            salario2 =double.Parse(Console.ReadLine(), CI);
+            //Console.WriteLine("Digite o salário da segunda pessoa: ");
+            //salario2 = double.Parse(Console.ReadLine(), CI);
 
-            Console.WriteLine("Digite uma idade: ");
-            idade=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite uma idade: ");
+            //idade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o sexo: ");
-            sexo=char.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o sexo: ");
+            //sexo = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Nome 1: " + nome1);
-            Console.WriteLine("Salário: " + salario1.ToString("F2", CI));
-            Console.WriteLine("Nome 2: " + nome2);
-            Console.WriteLine("Salário: " + salario2.ToString("F2", CI));
-            Console.WriteLine("Idade: "+idade);
-            Console.WriteLine("Sexo: "+sexo);
+            //Console.WriteLine("Nome 1: " + nome1);
+            //Console.WriteLine("Salário: " + salario1.ToString("F2", CI));
+            //Console.WriteLine("Nome 2: " + nome2);
+            //Console.WriteLine("Salário: " + salario2.ToString("F2", CI));
+            //Console.WriteLine("Idade: " + idade);
+            //Console.WriteLine("Sexo: " + sexo);
 
+            // Debbuger
+            //int x, y, z;
+            //x=int.Parse(Console.ReadLine());
+            //Console.WriteLine("X = " + x);
+            //y = x * 2;
+            //Console.WriteLine("Y = " + y);
+            //z=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Z = " + z);
+
+            // Estrutura condicional - Repetição
+            // Simples - if(condicao){comando}
+            // Composta - if(){} else{comando}
+            // Encadeamento - if(){} else if(){} else{}
+            //char opc = 's';
+            //while (opc != 'n') {
+            //    Console.WriteLine("Digite uma hora do dia: ");
+            //    int hora;
+            //    hora = int.Parse(Console.ReadLine());
+            //    if (hora >= 6 && hora < 12)
+            //    {
+            //        Console.WriteLine("Bom dia!");
+            //    }
+            //    else if (hora >= 12 && hora < 18)
+            //    {
+            //        Console.WriteLine("Boa tarde!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Boa noite!");
+            //    }
+            //    Console.WriteLine("Deseja continuar? (s/n): ");
+            //    opc = char.Parse(Console.ReadLine());
+            //}           
+
+            // Vetores
+            //Console.WriteLine("Quantos números você quer inserir? ");
+            //int n=int.Parse(Console.ReadLine());
+            //double[] vet = new double[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine("Número "+i+1+": ");
+            //    vet[i] = double.Parse(Console.ReadLine(), CI);
+            //}
+            //Console.WriteLine("Números: ");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write(" "+vet[i]);
+            //}
+            //Console.WriteLine();
+
+            // Matrizes
+            //Console.WriteLine("Quantidade de linhas: ");
+            //int linhas = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Quantidade de colunas: ");
+            //int colunas = int.Parse(Console.ReadLine());
+            //int[,] matriz = new int[linhas, colunas];
+            //for (int i = 0; i < linhas; i++)
+            //{
+            //    for (int j = 0; j < colunas; j++)
+            //    {
+            //        Console.WriteLine("Elemento [" + i + "," + j + "]: ");
+            //        matriz[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Matriz: ");
+            //for (int i = 0; i < linhas; i++)
+            //{
+            //    for (int j = 0; j < colunas; j++)
+            //    {
+            //        Console.Write(matriz[i, j]+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
         }
     }
 }
